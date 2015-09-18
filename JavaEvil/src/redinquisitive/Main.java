f3854db885e72402fbfb488a67848be33d270889
RedInquisitive <aj.walter@aol.com>
2015-09-18 11:25:51 -0500

    Yeah

--- JavaEvil/src/redinquisitive/Main.java
+++ JavaEvil/src/redinquisitive/Main.java
@@ -1,10 +1,19 @@
 package redinquisitive;
 
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//New Repo
-
+		System.out.print("Start: ");
+		String read = "";
+		while(!(read.contains("\n"))) {
+			try {
+				read += (char)(System.in.read());
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		}
+		System.out.print(read);
 	}
-
 }
